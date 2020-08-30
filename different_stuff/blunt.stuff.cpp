499e0e538cbb41f6c5a90bfb3cf0dbd5fded2c11
Ruslan <rv.volovik88@gmail.com>
2020-08-30 12:05:54 +0300

    blunt and dull stuff for test code examples

--- different_stuff/blunt.stuff.cpp
+++ different_stuff/blunt.stuff.cpp
@@ -22,6 +22,15 @@ constexpr double ext = ((((const double)exp(e_i) / 0.789651) / exp(e_i * 12.21))
          (((exp(e_i / 2914.124) * e_i) / exp(e_i / 56.9137)) * exp(e_i * 139.91470));
 constexpr double f_expr() { return exp((CPP_PI * 10.9) / 21.13); }
 
+enum color { RED, GREEN, BLUE, SOME };
+enum class tank_direction { UP, DOWN, RIGHT, LEFT };
+enum color operator++(enum color &r_o) {
+  r_o = r_o == SOME ? RED : color(int(r_o) + 1);
+};
+enum tank_direction operator++(enum tank_direction &r_t) {
+  r_t = r_t == tank_direction::DOWN ? tank_direction::UP : tank_direction(int(r_t) + 1);
+};
+
 enum error_file_c { EFE_UNKNOWN = 0, EFE_NO, EFE_OPEN, CLOSE, EFE_CRASH };
 enum error_network_c { ENE_UNKNOWN = 0, ENE_NO, OPEN, ENE_CLOSE, ENE_LOST, ENE_CRASH };
 namespace f_ns { int flag = 0; enum error_file_c : unsigned short int { UNKNOWN = 0, NO, OPEN, CLOSE, CRASH }; };
@@ -61,6 +70,33 @@ int main(int argc, char const **argv) {
   n_ns::error_network_c n_n_f_a = n_ns::UNKNOWN;
   error_file_f_s n_e_f_b = error_file_f_s::UNKNOWN;
   error_network_n_s n_n_f_b = error_network_n_s::UNKNOWN;
+
+  {
+    enum color color_a{RED};
+    enum color color_b(RED);
+    enum color color_c = RED;
+    enum color color_d = color{RED};
+    enum color color_e = color(RED);
+    unsigned i = color_a; i = RED;
+    color_a = (enum color)i;
+    color_a = color(i);
+    const unsigned h = (unsigned)++color_a;
+    const unsigned g = (unsigned)++color_a;
+    std::cout << h << " " << g << std::endl;
+  }
+  {
+    tank_direction et_dir_a{tank_direction::UP};
+    tank_direction et_dir_b(tank_direction::DOWN);
+    tank_direction et_dir_c = tank_direction(tank_direction::RIGHT);
+    tank_direction et_dir_d = tank_direction{tank_direction::LEFT};
+    unsigned i = 0;
+    i = int(tank_direction::UP);
+    i = (int)(tank_direction::RIGHT);
+    et_dir_a = tank_direction(i);
+    const unsigned h = (unsigned)++et_dir_a;
+    const unsigned g = (unsigned)++et_dir_a;
+    std::cout << h << " " << g << std::endl;
+  }
 
   using namespace f_ns; flag = 1;
   using namespace n_ns; ns_test(&i, r_i);
