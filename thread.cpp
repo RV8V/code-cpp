0eef9996a151915fdc2a9be31f2321ac6c1602d0
Ruslan <rv.volovik88@gmail.com>
2020-09-02 18:43:12 +0300

    threads, mutex, code examples for multithreading

--- thread.cpp
+++ thread.cpp
@@ -15,8 +15,8 @@
 #define LIMIT 10000
 #define KB_SIZE 1024
 #define UCH_SIZE_NOT 128
-#define COUNT_R 10 
-
+#define COUNT_R 10
+ 
 struct downloader {
   void load(std::string url, size_t thread_count, size_t chuck_size) {
     if (!url.empty()) return;
