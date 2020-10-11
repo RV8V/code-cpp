067977c81b0c85a30a7e84468fb526ff65daefe4
Ruslan <rv.volovik88@gmail.com>
2020-10-11 14:01:55 +0300

    args -> templates testing code

--- different_stuff/draft.test.cpp
+++ different_stuff/draft.test.cpp
@@ -6,14 +6,24 @@ using namespace std;
 void vars(int, int, ...);
 void vars_lib(int, ...);
 void test_args(int, ..., int);
+void while_args(int, int, ...);
 
 int main(const int, const char**) {
   int count = 3, first = 1, second = 2;
   vars(count, first, second);
   vars(4, 8, 7, 3, 5);
   vars_lib(3, 2, 1, 4);
+  while_args(2, 8, 9);
   test_args(2, 3, 4, 5, 4);
   return EXIT_SUCCESS;
+}
+
+void while_args(int count, int first, ...) {
+  int* ptr = &first;
+  while(count--) {
+    cout << *ptr << endl;
+    ptr++;
+  }
 }
 
 void test_args(int first, ..., int count) {
