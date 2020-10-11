9206317b0f4c0eb0484ab5b38bb59a18b4b44105
Ruslan <rv.volovik88@gmail.com>
2020-10-11 13:52:45 +0300

    args -> templates testing code

---
+++ different_stuff/draft.test.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stdarg.h>
+
+using namespace std;
+
+void vars(int, int, ...);
+void vars_lib(int, ...);
+void test_args(int, ..., int);
+
+int main(const int, const char**) {
+  int count = 3, first = 1, second = 2;
+  vars(count, first, second);
+  vars(4, 8, 7, 3, 5);
+  vars_lib(3, 2, 1, 4);
+  test_args(2, 3, 4, 5, 4);
+  return EXIT_SUCCESS;
+}
+
+void test_args(int first, ..., int count) {
+  int* ptr = &first;
+  for (int i = 0; i < count; ++i)
+    cout << "current: " << *(ptr + i) << endl;
+}
+
+void vars(int num, int first, ...) {
+  int* ptr = &first;
+  int i = 0;
+  do {
+    cout << "number: " << num << " " << "value -> " << *(ptr + i) << endl;
+    i++;
+  } while(num-- > 1);
+}
+
+void vars_lib(int count, ...) {
+  va_list args;
+  va_start(args, count);
+  do {
+    cout << "value is: " << va_arg(args, int) << endl;
+  } while(count-- > 1);
+  va_end(args);
+}
