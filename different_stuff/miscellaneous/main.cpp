6f43e21b066cddbb5ad7e4fbb124c583272c6afc
Ruslan <rv.volovik88@gmail.com>
2020-10-13 13:51:45 +0300

    misc -> templates testing

---
+++ different_stuff/miscellaneous/main.cpp
@@ -0,0 +1,10 @@
+#pragma once
+
+class this_is_a_class {
+private:
+public:
+};
+
+int b(int x) {
+  return x;
+}
---
+++ different_stuff/miscellaneous/source.cpp
@@ -0,0 +1,9 @@
+#include "main.cpp"
+
+int f(int x) {
+  return b(x);
+}
+
+int main(int, const char**) {
+  return 0;
+}
