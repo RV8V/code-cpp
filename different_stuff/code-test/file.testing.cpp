bfbf488bf15e99a80ba00a5b7e3a72d9ff8502ad
Ruslan <rv.volovik88@gmail.com>
2020-10-22 14:36:01 +0300

    list -> testing code -> templates

---
+++ different_stuff/code-test/file.testing.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+template<typename T>
+class my_list {
+public:
+  my_list();
+  ~my_list();
+
+  void push_back(T data);
+  int get_size();
+
+  T& operator[](const int);
+  T& operator+(const int);
+private:
+  template<typename V>
+  class node {
+  public:
+    node* p_next;
+    V data;
+
+    node(V data = V(), node* p_next = nullptr) {
+      this->data = data;
+      this->p_next = p_next;
+    }
+  };
+
+  node<T>* head = nullptr;
+  int size;
+};
+
+template<typename T>
+my_list<T>::my_list() {
+  this->size = 0;
+  this->head = nullptr;
+}
+
+template<typename T>
+void my_list<T>::push_back(T data) {
+  if (this->head == nullptr)
+    this->head = new node<T>(data);
+  else {
+    node<T>* current = this->head;
+    while(current->p_next != nullptr)
+      current = current->p_next;
+    current->p_next = new node<T>(data);
+  }
+  ++this->size;
+}
+
+template<typename T>
+T& my_list<T>::operator+(const int index) {
+  int counter = 0;
+  node<T>* current = this->head;
+  assert(index < this->size && "no such index in current list");
+  while(current != nullptr) {
+    if (counter == index)
+      return current->data;
+    else
+      current = current->p_next;
+    ++counter;
+  }
+}
+
+template<typename T>
+int my_list<T>::get_size() {
+  return this->size;
+}
+
+template<typename T>
+T& my_list<T>::operator[](const int index) {
+  int counter = 0;
+  node<T>* current = this->head;
+  assert(index < this->size && "no such index in current list");
+  while(current != nullptr) {
+    if (counter == index)
+      return current->data;
+    else
+      current = current->p_next;
+    ++counter;
+  }
+}
+
+template<typename T>
+my_list<T>::~my_list() {
+}
+
+int main(int, const char**) {
+  my_list<int> list;
+  int first = 10, second = 20;
+  list.push_back(first);
+  list.push_back(second);
+
+  cout << "size: " << list.get_size() << "\nvalue: " << list[1] << endl;
+
+  int i = list.get_size() - 1;
+  do {
+    cout << "current value in list -> " << list + i << endl;
+  } while(i-- > 0);
+  return EXIT_SUCCESS;
+}
