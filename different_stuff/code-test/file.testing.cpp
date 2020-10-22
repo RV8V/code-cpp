ab5f9d18c2555cb9419afb4c4d393184da3bb083
Ruslan <rv.volovik88@gmail.com>
2020-10-22 21:53:26 +0300

    some testing code -> templates, list

--- different_stuff/code-test/file.testing.cpp
+++ different_stuff/code-test/file.testing.cpp
@@ -14,6 +14,11 @@ public:
   int get_size();
   void clear();
 
+  void push_front(T);
+  void pop_back();
+  void insert(T, const int);
+  void remove_at(const int);
+
   T& operator[](const int);
   T& operator+(const int);
 private:
@@ -100,6 +105,51 @@ T& my_list<T>::operator[](const int index) {
 }
 
 template<typename T>
+void my_list<T>::push_front(T data) {
+  this->head = new node<T>(data, this->head);
+  ++this->size;
+}
+
+template<typename T>
+void my_list<T>::insert(T data, const int index) {
+  if (index == 0)
+    this->push_front(data);
+  else {
+    node<T>* previous = this->head;
+    for (int i = 0; i < index - 1; ++i) {
+      cout << "fails here, why" << endl;
+      previous = previous->p_next;
+    }
+
+    node<T>* new_node = new node<T>(data, previous->p_next);
+    previous->p_next = new_node;
+    ++this->size;
+  }
+}
+
+template<typename T>
+void my_list<T>::remove_at(const int index) {
+  if (index == 0)
+    this->pop_front();
+  else {
+    node<T>* previous = this->head;
+    for (int i = 0; i < index - 1; ++i) {
+      previous = previous->p_next;
+    }
+
+    node<T>* node_to_delete = previous->p_next;
+    previous->p_next = node_to_delete->p_next;
+    delete node_to_delete;
+    --this->size;
+  }
+}
+
+template<typename T>
+void my_list<T>::pop_back() {
+  this->remove_at(this->size - 1);
+}
+
+template<typename T>
 my_list<T>::~my_list() {
   this->clear();
 }
@@ -109,6 +159,8 @@ int main(int, const char**) {
   int first = 10, second = 20;
   list.push_back(first);
   list.push_back(second);
+
+  list.push_front(9);
 
   cout << "size: " << list.get_size() << "\nvalue: " << list[1] << endl;
 
@@ -124,6 +176,24 @@ int main(int, const char**) {
   cout << "list size: " << list.get_size() << endl;
   list.clear();
   cout << "list size: " << list.get_size() << endl;
+
+#ifdef insert_testing
+  cout << "insert\n";
+  list.insert(80, 2);
+  list.insert(34, 1);
+
+  for (int i = 0; i < list.get_size(); ++i)
+    cout << "current value -> " << list + i << endl;
+#endif
+
+#ifdef remove_at_testing
+  list.push_front(102);
+  list.push_front(201);
+
+  cout << "remove at\n";
+  list.remove_at(0);
+  list.remove_at(2);
+#endif
 
   return EXIT_SUCCESS;
 }
